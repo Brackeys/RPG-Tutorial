5cf1e83e846958abb8a3fc1930456efa579a29a0
Asbjørn Thirslund <at@brackeys.com>
2017-06-26 15:28:24 +0200

    Update Camera Controller

--- RPG Tutorial/Assets/Scripts/Controllers/CameraController.cs
+++ RPG Tutorial/Assets/Scripts/Controllers/CameraController.cs
@@ -5,40 +5,26 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
 	public Transform target;
+
 	public Vector3 offset;
+	public float smoothSpeed = 2f;
 
 	public float currentZoom = 1f;
-	public float maxZoom = 3f;
 	public float minZoom = .3f;
-	public float yawSpeed = 70;
+	public float maxZoom = 3f;
 	public float zoomSensitivity = .7f;
-	float dst;
-
-	float zoomSmoothV;
-	float targetZoom;
-
-	void Start() {
-		dst = offset.magnitude;
-		transform.LookAt (target);
-		targetZoom = currentZoom;
-	}
 
 	void Update ()
 	{
-		float scroll = Input.GetAxisRaw("Mouse ScrollWheel") * zoomSensitivity;
-
-		if (scroll != 0f)
-		{
-			targetZoom = Mathf.Clamp(targetZoom - scroll, minZoom, maxZoom);
-		}
-		currentZoom = Mathf.SmoothDamp (currentZoom, targetZoom, ref zoomSmoothV, .15f);
+		currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+		currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 	}
 
 	void LateUpdate () {
-		transform.position = target.position - transform.forward * dst * currentZoom;
-		transform.LookAt(target.position);
+		Vector3 desiredPos = target.position - (target.forward + offset) * currentZoom;
+		Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * smoothSpeed);
+        transform.position = smoothedPos;
 
-		float yawInput = Input.GetAxisRaw ("Horizontal");
-		transform.RotateAround (target.position, Vector3.up, -yawInput * yawSpeed * Time.deltaTime);
+		transform.LookAt(target.position);
 	}
 }

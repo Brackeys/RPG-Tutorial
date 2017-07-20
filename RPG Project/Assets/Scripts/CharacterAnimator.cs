5bf80e1c3753b27e69b72fa83c491587f65b64a0
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-07-20 11:07:00 +0200

    Added files for episode 03 (player graphics integration)

---
+++ RPG Project/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomationAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
+	}
+}
